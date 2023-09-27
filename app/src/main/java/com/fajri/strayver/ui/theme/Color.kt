620a1877d568c6f382d927071c3f2634f4367407
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2023-09-27 12:21:56 +0700

    first commit

---
+++ app/src/main/java/com/fajri/strayver/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.fajri.strayver.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Neutral
+val Neutral50 = Color(0xFFF8FAFC)
+val Neutral100 = Color(0xFFF1F5F9)
+val Neutral200 = Color(0xFFE2E8F0)
+val Neutral300 = Color(0xFFCBD5E1)
+val Neutral400 = Color(0xFF94A3B8)
+val Neutral500 = Color(0xFF64748B)
+val Neutral600 = Color(0xFF475569)
+val Neutral700 = Color(0xFF334155)
+val Neutral800 = Color(0xFF1E293B)
+val Neutral900 = Color(0xFF0F172A)
+
+// Primary (Cyan)
+val Cyan50 = Color(0xFFEBFAFD)
+val Cyan100 = Color(0xFFD7F5FB)
+val Cyan200 = Color(0xFFC2F0F9)
+val Cyan300 = Color(0xFFAEEBF7)
