6930af8051237386ee7efa3571e9f05154b4f7c2
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2023-10-28 23:05:04 +0700

    fix

--- app/src/main/java/com/fajri/strayver/data/FirebaseRepository.kt
+++ app/src/main/java/com/fajri/strayver/data/FirebaseRepository.kt
@@ -8,5 +8,6 @@ import com.google.firebase.database.database
 class FirebaseRepository() {
     private var auth= Firebase.auth
     private var db= Firebase.database.reference
+    private val gbuigbui= 0
 
 }
