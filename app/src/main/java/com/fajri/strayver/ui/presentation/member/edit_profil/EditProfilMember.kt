a152bcb51bce2f63493a390783d690f654b1e10e
SyafiIslam <muhammadsyafiislam@gmail.com>
2023-11-09 16:21:27 +0700

    update edit profile ui

--- app/src/main/java/com/fajri/strayver/ui/presentation/member/edit_profil/EditProfilMember.kt
+++ app/src/main/java/com/fajri/strayver/ui/presentation/member/edit_profil/EditProfilMember.kt
@@ -1,12 +1,20 @@
 package com.fajri.strayver.ui.presentation.member.edit_profil
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,17 +37,35 @@ fun EditProfilMemberScreen() {
             .background(Primary700)
     ) {
         AsyncImage(model = R.drawable.ilustrasi2, contentDescription = "")
-        Box(
+        Row(
             Modifier
                 .align(Alignment.TopCenter)
                 .offset(y = 60.dp)
                 .clip(CircleShape)
                 .background(Shades50)
-                .padding(vertical = 4.dp, horizontal = 24.dp)
+                .padding(vertical = 4.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
         ) {
+
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIosNew,
+                    contentDescription = "",
+                    tint = Primary700,
+                    modifier = Modifier
+                        .padding(bottom = 3.dp)
+                        .size(16.dp)
+                        .clip(CircleShape)
+                )
+            }
+
             Text(
                 text = "Edit Profil",
-                style = Type.textLgSemiBold(),
+                style = Type.textLgBold(),
                 color = Primary700,
             )
         }
--- app/src/main/java/com/fajri/strayver/ui/presentation/member/profil/MemberProfilScreen.kt
+++ app/src/main/java/com/fajri/strayver/ui/presentation/member/profil/MemberProfilScreen.kt
@@ -1,9 +1,57 @@
 package com.fajri.strayver.ui.presentation.member.profil
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.fajri.strayver.R
+import com.fajri.strayver.ui.presentation.component.ProfilePicture
+import com.fajri.strayver.ui.presentation.member.edit_profil.component.EditProfilContent
+import com.fajri.strayver.ui.theme.Primary700
+import com.fajri.strayver.ui.theme.Shades50
+import com.fajri.strayver.ui.theme.Type
 
 @Composable
 fun MemberProfilScreen() {
-    Text(text = "MemberProfilScreen")
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(Primary700)
+    ) {
+        AsyncImage(model = R.drawable.ilustrasi2, contentDescription = "")
+        Box(
+            Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = 60.dp)
+                .clip(CircleShape)
+                .background(Shades50)
+                .padding(vertical = 4.dp, horizontal = 24.dp)
+        ) {
+            Text(
+                text = "Edit Profil",
+                style = Type.textLgSemiBold(),
+                color = Primary700,
+            )
+        }
+
+        Column(Modifier.align(Alignment.TopStart)) {
+            EditProfilContent()
+        }
+
+        ProfilePicture(
+            Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = 115.dp)
+        )
+    }
 }
